28f505ca513136fae3e48f2ea5e69b0028bddeac
AlexEbral <aleksandr.ebr@gmail.com>
2021-08-10 22:32:53 +0300

    Revert "fix: minsize"
    This reverts commit eb348eb112449582dc7415be3b00b3a2d1e07d12.

--- aiossdb/pool.py
+++ aiossdb/pool.py
@@ -6,7 +6,7 @@ from .errors import PoolClosedError
 from .log import logger
 
 
-async def create_pool(address, *, password=None, encoding='utf-8', minsize=0, maxsize=10,
+async def create_pool(address, *, password=None, encoding='utf-8', minsize=1, maxsize=10,
                       parser=None, loop=None, timeout=None, pool_cls=None, connection_cls=None):
     if pool_cls is None:
         pool_cls = SSDBConnectionPool
